642e0e8bcf0bd89aedc6b94e326504e8f862ac7e
Anandbalakrishna <anand.balakrishna@kearney.com>
2025-12-07 05:24:53 +0530

    Update button text to 'Idea Submitting...'

--- src/App.js
+++ src/App.js
@@ -232,10 +232,6 @@ const IdeaVault = () => {
                 <Shield className="w-5 h-5 text-orange-500 flex-shrink-0 mt-0.5" />
                 <div>
                   <p className="text-sm text-gray-300">Your submission is completely anonymous. We value all ideas!</p>
-                  <p className="text-xs text-gray-500 mt-1 flex items-center gap-1">
-                    <Database className="w-3 h-3" />
-                    Ideas are saved to Google Sheets
-                  </p>
                 </div>
               </div>
             </div>
@@ -299,7 +295,7 @@ const IdeaVault = () => {
                 {isSubmitting ? (
                   <>
                     <Sparkles className="w-5 h-5 animate-spin" />
-                    Saving to Google Sheets...
+                    Idea Submitting...
                   </>
                 ) : (
                   <>
